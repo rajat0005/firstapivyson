bc79d1125dba0270433bd3ea38936b3e76c48b3e
rajat0005 <rkamra94@gmail.com>
2025-06-06 22:09:54 +0530

    post api working

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 const express = require('express');
+// const sequelize = require('sequelize');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
 });
 app.use(express.json());
-const jobTypes = [
-    { id: 1, type: "Interior" },
-    { id: 2, type: "Exterior" },
-    { id: 3, type: "Roof" },
-    { id: 4, type: "Renovations" },
-    { id: 5, type: "Roof" },
-];
 
-app.post("/api/jobtypes", (req, res) => {
-    const jobtype = { id: jobTypes.length + 1, type: req?.body?.type || null };
-    jobTypes.push(jobtype);
-    res.send(jobTypes);
+app.post("/shorten", (req, res) => {
+    const shortUrl = `tinyurl/`+req?.body?.longurl.substring(4,7);
+    res.send(shortUrl);
 });
