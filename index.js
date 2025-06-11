a2b85b05ae746a818f1b37616d1f53143de29ddd
rajat0005 <rkamra94@gmail.com>
2025-06-11 12:32:23 +0530

    initial setup apis

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express');
 // const sequelize = require('sequelize');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log("Server Listening on PORT:", PORT);
 });
 app.use(express.json());
-
+let shortUrl = '';
+let longUrl = '';
 app.post("/shorten", (req, res) => {
-    const shortUrl = `tinyurl/`+req?.body?.longurl.substring(4,7);
+    longUrl = req?.body?.longurl;
+    shortUrl = `tinyurl/` + longUrl.substring(4, 8);
     res.send(shortUrl);
 });
+
+app.get("/redirect", (req, res) => {
+    console.log(req?.query?.code, ':::::', longUrl);
+    if (req?.query?.code == shortUrl) {
+        require("openurl").open(longUrl);
+    }
+})
